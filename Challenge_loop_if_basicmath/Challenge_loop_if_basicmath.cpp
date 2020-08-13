3aa88ec7f8c43dcc7ce29fd6c74c4b1826ce86a9
folnw <folnw@LAPTOP-3GBNUI50>
2020-08-13 08:35:40 +0700

    finished program

--- Challenge_loop_if_basicmath/Challenge_loop_if_basicmath.cpp
+++ Challenge_loop_if_basicmath/Challenge_loop_if_basicmath.cpp
@@ -2,24 +2,35 @@
 #include<math.h>
 int main() {
 	float start, finish;
-	scanf("%f %f", &start, &finish); //input
-	float average = (((start + finish) * (abs(finish - start) + 1)) / 2.0) / (abs(finish - start) + 1); //average = sum/count of data
-	float sumforsd; //used for find S.D.
+	scanf_s("%f %f", &start, &finish); //input
+	float average;
+	if (start < finish) {
+		average = (((start + finish)*((finish - start) + 1)) / 2.0) / ((finish-start) + 1);
+	}
+	else if (start > finish) {
+		average = (((start + finish)*((start - finish) + 1)) / 2.0) / ((start-finish) + 1);
+	}
+	float sumforsd = 0; //used for find S.D.
 	if (start < finish)//start value less than finish value cases
 	{
 		for (int i = start; i <= finish; i++) {
 			printf("%d ", i);
-			sumforsd += pow((i - average), 2); //i - average = delta value
+			sumforsd += pow((i-average), 2); //i - average = delta value
 		}
 	}
 	else if (start > finish)//start value more than finish value cases
 	{
 		for (int i = start; i >= finish; i--) {
 			printf("%d ", i);
-			sumforsd += pow((i - average), 2); //i - average = delta value
+			sumforsd += pow((i-average), 2); //i - average = delta value
 		}
 	}
-	printf("\nAverage = %.2f", average); //average
-	printf("\nSD = %.2f", sqrt(sumforsd / abs(finish - start))); //S.D.
+	printf("\nAverage = %.2f",average); //average
+	if (start < finish) {
+		printf("\nSD = %.2f", sqrt(sumforsd /(finish - start))); //S.D.
+	}
+	else if (start > finish) {
+		printf("\nSD = %.2f", sqrt(sumforsd /(start - finish))); //S.D.
+	}
 	return 0;
 }
